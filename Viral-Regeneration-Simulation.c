5b31019d46ba97391ac9d356577109822c48e6e0
Francisritchie <francisritchiecauba@gmail.com>
2025-04-01 12:17:07 +0800

    Update Viral-Regeneration-Simulation.c

--- Viral-Regeneration-Simulation.c
+++ Viral-Regeneration-Simulation.c
@@ -1,86 +1,114 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <time.h>
 
-#define SIZE 20
-#define ALIVE 'X'
+#define SIZE 30
+#define VIRUS 'V'
 #define DEAD '.'
-#define DELAY 200000
 
-void initializeGrid(char grid[SIZE][SIZE]);
-void displayGrid(char grid[SIZE][SIZE]);
-int countNeighbors(char grid[SIZE][SIZE], int x, int y);
-void updateGrid(char grid[SIZE][SIZE], char newGrid[SIZE][SIZE]);
-void copyGrid(char grid[SIZE][SIZE], char newGrid[SIZE][SIZE]);
-
-int main() {
-    srand(time(NULL));
-    char grid[SIZE][SIZE];
-    char newGrid[SIZE][SIZE];
-
-    initializeGrid(grid);
-    while (1) {
-        displayGrid(grid);
-        updateGrid(grid, newGrid);
-        copyGrid(newGrid, grid);
-        usleep(DELAY);
-    }
-
-    return 0;
-}
-
-void initializeGrid(char grid[SIZE][SIZE]) {
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            grid[i][j] = (rand() % 2) ? ALIVE : DEAD;
+void setupVirus(int virusRow, char grid[SIZE][SIZE]) {
+    
+    for (int row = 0; row < SIZE; row++) {
+        for (int col = 0; col < SIZE; col++) {
+            grid[row][col] = DEAD;  //
         }
     }
+    
+    for (int col = 0; col < SIZE; col++) {
+        grid[virusRow][col] = VIRUS;  
+    }
 }
 
-void displayGrid(char grid[SIZE][SIZE]) {
-    printf("\033[H");
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            printf("%c ", grid[i][j]);
+void printGrid(char grid[SIZE][SIZE]) {
+    
+    for (int row = 0; row < SIZE; row++) {
+        for (int col = 0; col < SIZE; col++) {
+            printf("%c ", grid[row][col]);
         }
         printf("\n");
     }
 }
 
-int countNeighbors(char grid[SIZE][SIZE], int x, int y) {
-    int count = 0;
+int countNeighbors(char grid[SIZE][SIZE], int row, int col) {
+    
+    int neighbors = 0;
     for (int i = -1; i <= 1; i++) {
         for (int j = -1; j <= 1; j++) {
-            if (i == 0 && j == 0) continue;
-            int nx = x + i, ny = y + j;
-            if (nx >= 0 && nx < SIZE && ny >= 0 && ny < SIZE) {
-                if (grid[nx][ny] == ALIVE) count++;
+            if (i == 0 && j == 0) continue;  
+            int nRow = row + i, nCol = col + j;
+            if (nRow >= 0 && nRow < SIZE && nCol >= 0 && nCol < SIZE) {
+                if (grid[nRow][nCol] == VIRUS) {
+                    neighbors++;
+                }
             }
         }
     }
-    return count;
+    return neighbors;
 }
 
 void updateGrid(char grid[SIZE][SIZE], char newGrid[SIZE][SIZE]) {
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            int neighbors = countNeighbors(grid, i, j);
-            if (grid[i][j] == ALIVE) {
-                if (neighbors < 2 || neighbors > 3) newGrid[i][j] = DEAD;
-                else newGrid[i][j] = ALIVE;
+    
+    for (int row = 0; row < SIZE; row++) {
+        for (int col = 0; col < SIZE; col++) {
+            int neighbors = countNeighbors(grid, row, col);
+
+            if (grid[row][col] == VIRUS) {
+                
+                newGrid[row][col] = (neighbors < 2 || neighbors > 3) ? DEAD : VIRUS;
             } else {
-                if (neighbors == 3) newGrid[i][j] = ALIVE;
-                else newGrid[i][j] = DEAD;
+              
+                newGrid[row][col] = (neighbors == 3) ? VIRUS : DEAD;
             }
         }
     }
 }
 
 void copyGrid(char grid[SIZE][SIZE], char newGrid[SIZE][SIZE]) {
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            grid[i][j] = newGrid[i][j];
+    
+    for (int row = 0; row < SIZE; row++) {
+        for (int col = 0; col < SIZE; col++) {
+            grid[row][col] = newGrid[row][col];
         }
     }
+}
+
+void clearScreen() {
+   
+    #ifdef _WIN32
+        system("cls");
+    #else
+        printf("\033[H\033[J");
+    #endif
+}
+
+int main() {
+    char grid[SIZE][SIZE], newGrid[SIZE][SIZE];
+    int virusRow;
+
+    
+    printf("Enter the row number (0-%d) where the virus should start: ", SIZE - 1);
+    scanf("%d", &virusRow);
+
+    
+    if (virusRow < 0 || virusRow >= SIZE) {
+        printf("Invalid row. Exiting...\n");
+        return 1;
+    }
+
+    
+    setupVirus(virusRow, grid);
+
+    
+    while (1) {
+        clearScreen();  /
+
+        printGrid(grid);  
+
+        updateGrid(grid, newGrid);  
+        copyGrid(grid, newGrid);    
+
+        sleep(1);  
+    }
+
+    return 0;
 }
