78e0037ac13ac32a3fa7f932e9c3b671dfb1c03b
Francisritchie <francisritchiecauba@gmail.com>
2025-04-01 11:32:30 +0800

    Update Viral-Regeneration-Simulation.c

--- Viral-Regeneration-Simulation.c
+++ Viral-Regeneration-Simulation.c
@@ -3,23 +3,21 @@
 #include <unistd.h>
 #include <time.h>
 
-
 #define SIZE 20
 #define ALIVE 'X'
 #define DEAD '.'
-#define DELAY 200000 
+#define DELAY 200000
 
-
-void initializeGrid(int grid[SIZE][SIZE]);
-void displayGrid(int grid[SIZE][SIZE]);
-int countNeighbors(int grid[SIZE][SIZE], int x, int y);
-void updateGrid(int grid[SIZE][SIZE], int newGrid[SIZE][SIZE]);
-void copyGrid(int grid[SIZE][SIZE], int newGrid[SIZE][SIZE]);
+void initializeGrid(char grid[SIZE][SIZE]);
+void displayGrid(char grid[SIZE][SIZE]);
+int countNeighbors(char grid[SIZE][SIZE], int x, int y);
+void updateGrid(char grid[SIZE][SIZE], char newGrid[SIZE][SIZE]);
+void copyGrid(char grid[SIZE][SIZE], char newGrid[SIZE][SIZE]);
 
 int main() {
-    srand(time(NULL)); 
-    int grid[SIZE][SIZE];
-    int newGrid[SIZE][SIZE];
+    srand(time(NULL));
+    char grid[SIZE][SIZE];
+    char newGrid[SIZE][SIZE];
 
     initializeGrid(grid);
     while (1) {
@@ -32,59 +30,54 @@ int main() {
     return 0;
 }
 
-
-void initializeGrid(int grid[SIZE][SIZE]) {
+void initializeGrid(char grid[SIZE][SIZE]) {
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            grid[i][j] = rand() % 2;
+            grid[i][j] = (rand() % 2) ? ALIVE : DEAD;
         }
     }
 }
 
-
-void displayGrid(int grid[SIZE][SIZE]) {
-    printf("\033[H"); 
+void displayGrid(char grid[SIZE][SIZE]) {
+    printf("\033[H");
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            printf("%c ", grid[i][j] ? ALIVE : DEAD);
+            printf("%c ", grid[i][j]);
         }
         printf("\n");
     }
 }
 
-
-int countNeighbors(int grid[SIZE][SIZE], int x, int y) {
+int countNeighbors(char grid[SIZE][SIZE], int x, int y) {
     int count = 0;
     for (int i = -1; i <= 1; i++) {
         for (int j = -1; j <= 1; j++) {
-            if (i == 0 && j == 0) continue; 
+            if (i == 0 && j == 0) continue;
             int nx = x + i, ny = y + j;
             if (nx >= 0 && nx < SIZE && ny >= 0 && ny < SIZE) {
-                count += grid[nx][ny];
+                if (grid[nx][ny] == ALIVE) count++;
             }
         }
     }
     return count;
 }
 
-
-void updateGrid(int grid[SIZE][SIZE], int newGrid[SIZE][SIZE]) {
+void updateGrid(char grid[SIZE][SIZE], char newGrid[SIZE][SIZE]) {
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             int neighbors = countNeighbors(grid, i, j);
-            if (grid[i][j] == 1) {
-                if (neighbors < 2 || neighbors > 3) newGrid[i][j] = 0;
-                else newGrid[i][j] = 1;
+            if (grid[i][j] == ALIVE) {
+                if (neighbors < 2 || neighbors > 3) newGrid[i][j] = DEAD;
+                else newGrid[i][j] = ALIVE;
             } else {
-                if (neighbors == 3) newGrid[i][j] = 1;
-                else newGrid[i][j] = 0;
+                if (neighbors == 3) newGrid[i][j] = ALIVE;
+                else newGrid[i][j] = DEAD;
             }
         }
     }
 }
 
-
-void copyGrid(int grid[SIZE][SIZE], int newGrid[SIZE][SIZE]) {
+void copyGrid(char grid[SIZE][SIZE], char newGrid[SIZE][SIZE]) {
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             grid[i][j] = newGrid[i][j];
